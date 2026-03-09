0e894bb41fc11c5d23a88e81a59698422bf502e3
RDanMT <r.dan.m.t.27@gmail.com>
2026-03-08 19:41:28 -0600

    Agregando protección de datos y nuevo proyecto

--- backend/Pruebas/Prueba.js
+++ backend/Pruebas/Prueba.js
@@ -1,17 +1,17 @@
-fetch('http://localhost:5000/api/projects', {
+fetch('https://portafolio-rdanmt.onrender.com/api/projects', { // <--- Cambiamos localhost por Render
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-        title: "Travel Recommendations",
-        description: "Una aplicación web interactiva para descubrir, explorar y recomendar destinos turísticos increíbles alrededor del mundo.",
-        image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=2074",
-        techStack: ["HTML", "CSS", "JavaScript"],
-        liveLink: "https://rdanmt.github.io/travel-recommendations/",
-        githubLink: "https://github.com/RDanMT/travel-recommendations.git"
+        title: "Plataforma Escolar",
+        description: "Sistema de gestión educativa para el control de alumnos, maestros y calificaciones desarrollado en Java.",
+        image: "https://images.unsplash.com/photo-1501503069356-3c6b82a17d89?q=80&w=2070",
+        techStack: ["Java", "MySQL", "JDBC", "MVC"],
+        liveLink: "https://github.com/RDanMT/PlataformaEscolar.git",
+        githubLink: "https://github.com/RDanMT/PlataformaEscolar.git"
     })
 })
     .then(respuesta => respuesta.json())
-    .then(datos => console.log("¡ÉXITO! Tercer proyecto guardado:", datos))
+    .then(datos => console.log("¡ÉXITO! Proyecto guardado en la base de datos:", datos))
     .catch(error => console.error("Hubo un error:", error));
